e209d7de90659995cc43fb519526c4eab80c7f45
eva789q <contacto@almargen.cl>
2025-02-05 13:57:52 -0300

    agregue array, y funcions

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los amigos
+let amigos = [];
+
+// Función para agregar un amigo
+function agregarAmigo() {
+    let elementoHTML = document.getElementById('amigo');
+    let nombreAmigo = elementoHTML.value; 
+
+    // Validar que el nombre no esté vacío
+    if (nombreAmigo === '') {
+        alert('Por favor, ingresa un nombre');
+        return;
+    }
+
+    // Validar que el nombre no esté registrado
+    if (amigos.includes(nombreAmigo)) {
+        alert('Este amigo ya está registrado.');
+        return;
+    }
+
+    // Agregar el amigo al array
+    amigos.push(nombreAmigo);
+    elementoHTML.value = ''; // Limpiar el campo de texto
+
+}
+console.log([amigos]);
